242872c83334f422929fce2583121341d62789f4
Tomek <tomek-dziewulski@wp.pl>
2023-03-17 22:19:32 +0100

    Pierwszy testowy komit

---
+++ src/main/java/projektnaukagita/ProjektNaukaGitaApplication.java
@@ -0,0 +1,13 @@
+package projektnaukagita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektNaukaGitaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjektNaukaGitaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/projektnaukagita/ProjektNaukaGitaApplicationTests.java
@@ -0,0 +1,13 @@
+package projektnaukagita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektNaukaGitaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
